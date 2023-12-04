584393e76deacedbe97159da6596b7feb45f6dfc
popelev <popelev.fedor@gmail.com>
2023-12-05 04:07:35 +0600

    send ether via timelock

--- test/DAO.t.sol
+++ test/DAO.t.sol
@@ -16,6 +16,7 @@ contract DaoTest is Test {
 
         DAOVoteToken public token;
         TimelockController public timelock;
+        EtherReciever public etherReciever;
         DAO public DAO_GOV;
         address public deployer = makeAddr('deployer');
         address[] public proposers = [makeAddr('proposer1'), makeAddr('proposer2')];
@@ -32,12 +33,23 @@ contract DaoTest is Test {
 
 
         function setUp() public {
+
+                etherReciever = new EtherReciever();
                 vm.deal(deployer, 10_000 ether);
                 vm.startPrank(deployer);
                 token = new DAOVoteToken();
-                timelock = new TimelockController(0, proposers, executors, address(0));
+                timelock = new TimelockController(0, proposers, executors, deployer);
+                timelock._setRoleAdmin(role, adminRole);
                 DAO_GOV = new DAO(IVotes(token), timelock);
+
+                bytes32 PROPOSER_ROLE = keccak256("PROPOSER_ROLE");
+                bytes32 EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
+                bytes32 CANCELLER_ROLE = keccak256("CANCELLER_ROLE");
                 
+                timelock.grantRole(PROPOSER_ROLE , address(DAO_GOV));
+                timelock.grantRole(EXECUTOR_ROLE , address(DAO_GOV));
+                timelock.grantRole(CANCELLER_ROLE , address(DAO_GOV));
+
                 token.mint(users[0], 1_000_000);
                 token.mint(users[1], 1_000_000);
                 token.mint(users[2], 1_000_000);
@@ -45,6 +57,8 @@ contract DaoTest is Test {
                 vm.stopPrank();
 
                 vm.deal(address(DAO_GOV), 10_000 ether);
+                vm.deal(address(timelock), 10_000 ether);
+
                 vm.deal(users[0], 10_000 ether);
                 vm.deal(users[1], 10_000 ether);
                 vm.deal(users[2], 10_000 ether);
@@ -90,8 +104,6 @@ contract DaoTest is Test {
                 skip(2 days);
                 vm.roll(172801);
 
-                emit logUint(uint256(DAO_GOV.state(proposalId)));
-
                 vm.prank(users[0]);
                 DAO_GOV.castVote(proposalId, VOTE_NO);
 
@@ -106,4 +118,48 @@ contract DaoTest is Test {
                 assert(y == 1_000_000);
                 assert(a == 1_000_000);
         }
+
+         function test_executePropose() public {
+                vm.startPrank(proposers[0]);
+                _target.push(address(etherReciever));
+                _value.push(1 ether);
+                _calldata.push(bytes(''));
+                _description = "Send 1 ether to some contract";
+                uint256 proposalId = DAO_GOV.propose(_target, _value, _calldata, _description);
+                vm.stopPrank(); 
+
+                skip(2 days);
+                vm.roll(172801);
+
+                vm.prank(users[0]);
+                DAO_GOV.castVote(proposalId, VOTE_YES);
+
+                vm.prank(users[1]);
+                DAO_GOV.castVote(proposalId, VOTE_YES);
+                
+                vm.prank(users[2]);
+                DAO_GOV.castVote(proposalId, VOTE_YES);
+     
+                skip(6 days);
+                vm.roll(900000);
+                
+                bytes32 _descriptionHash = keccak256(bytes(_description));
+
+                vm.startPrank(executors[0]);
+                DAO_GOV.queue(_target, _value, _calldata, _descriptionHash);
+
+                skip(1 days);
+                vm.roll(900001);
+
+                DAO_GOV.execute(_target, _value, _calldata, _descriptionHash);
+                vm.stopPrank(); 
+        }
 }
+
+contract EtherReciever {
+        fallback() external payable {}
+
+        receive() external payable {}
+}
+
+                // hashProposal = keccak256(abi.encode(targets, values, calldatas, descriptionHash));
